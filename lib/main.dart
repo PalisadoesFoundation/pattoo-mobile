30802b949e5862ccf042a2a91e7c602a372074e7
KingToast98 <32875984+ShannonH98@users.noreply.github.com>
2020-06-27 20:04:34 -0500

    Updated Charts Screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:pattoomobile/views/pages/LoginScreen.dart';
 import 'package:pattoomobile/views/pages/HomeScreen.dart';
 import 'package:pattoomobile/views/pages/SettingsScreen.dart';
+import 'package:pattoomobile/views/pages/ChartScreen.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() => runApp(MaterialApp(
 
   initialRoute: '/',
   routes: {
-    '/': (context) => SettingsScreen(),
+    '/': (context) => MyHomePage(),
     '/HomeScreen': (context) => HomeScreen(),
   },
 
